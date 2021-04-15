4ba2bdd357c4c195635c59a13c1e901086a91912
Jeremy <baodman@icloud.com>
2021-04-15 21:24:26 +0200

    ''

---
+++ change.js
@@ -0,0 +1,15 @@
+
+function myFunction() {
+
+
+    var name = document.getElementById("name").innerHTML;
+
+    let n = name.toUpperCase();
+    return n;
+}
+
+var a = "Jeremy";
+
+var b = Number(1);
+var c = 3;
+console.log(c.toString()+b);
